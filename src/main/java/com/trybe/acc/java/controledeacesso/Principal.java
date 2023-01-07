b4be61defb28e664d93bb26713d8d1ce4da4859e
WBomfim <willianbomfim@hotmail.com>
2023-01-07 02:14:47 -0300

    feat: Criado a classe Principal - A classe possui o método que executa o sistema de controle de acesso, exibe as opções para o usuário, coleta os dados de entrada e exibe o relatório completo de análize do público visitante.

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,7 +1,12 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
- * Calsse Principal.
+ * Classe Principal.
  *
  */
 public class Principal {
@@ -10,6 +15,40 @@ public class Principal {
    * Método principal.
    */
   public static void main(String[] args) {
-    // ESCREVA SEU CÓDIGO AQUI
+    ArrayList<Integer> idades = new ArrayList<Integer>();
+    short opcao = 1;
+
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    while (opcao != 2) {
+      Mensagens.opcoes();
+      opcao = scan.nextShort();
+
+      if (opcao != 1 && opcao != 2) {
+        Mensagens.imprime("Entre com uma opção válida!");
+      }
+
+      if (opcao == 1) {
+        Mensagens.entradaIdade();
+        int idade = scan.nextInt();
+
+        idades.add(idade);
+        Mensagens.liberacao(idade);
+      }
+    }
+
+    HashMap<String, Integer> quantidades = Calculadora.calcularQuantidades(idades);
+    HashMap<String, String> percentuais = Calculadora.calcularPercentual(idades);
+
+    Mensagens.relatorioQuantidade(quantidades.get("menores"), quantidades.get("adultos"),
+        quantidades.get("idosos"));
+
+    Mensagens.relatorioPercentual(percentuais.get("menores"), percentuais.get("adultos"),
+        percentuais.get("idosos"));
+
+    Mensagens.totalPessoas(idades.size());
+
+    scan.close();
   }
+
 }
