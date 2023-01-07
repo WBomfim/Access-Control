781bc925b44fcb6f7d7cd8deaee5fcd689d5c654
WBomfim <willianbomfim@hotmail.com>
2023-01-07 02:01:39 -0300

    refactor: Alterado o método calcularQuantidade da classe Calculadora - Alterado o tipo do parametro da função para ArrayList, sendo possivel passar o array de idades diretamente para o método.

--- src/main/java/com/trybe/acc/java/controledeacesso/Calculadora.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Calculadora.java
@@ -1,6 +1,7 @@
 package com.trybe.acc.java.controledeacesso;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -12,26 +13,26 @@ public class Calculadora {
    * Método calcularQuantidade.
    * 
    */
-  public static HashMap<String, Integer> calcularQuantidade(short[] idades) {
+  public static HashMap<String, Integer> calcularQuantidades(ArrayList<Integer> idades) {
     int menores = 0;
     int adultos = 0;
     int idosos = 0;
 
-    for (int index = 0; index < idades.length; index += 1) {
-      if (idades[index] < 18) {
+    for (int index = 0; index < idades.size(); index += 1) {
+      if (idades.get(index) < 18) {
         menores += 1;
       }
 
-      if (idades[index] >= 18 && idades[index] < 50) {
+      if (idades.get(index) >= 18 && idades.get(index) < 50) {
         adultos += 1;
       }
 
-      if (idades[index] >= 50) {
+      if (idades.get(index) >= 50) {
         idosos += 1;
       }
     }
 
-    HashMap<String, Integer> totais = new HashMap<>();
+    HashMap<String, Integer> totais = new HashMap<String, Integer>();
     totais.put("menores", menores);
     totais.put("adultos", adultos);
     totais.put("idosos", idosos);
@@ -43,19 +44,25 @@ public class Calculadora {
    * Método calcularPercentual.
    * 
    */
-  public static HashMap<String, String> calcularPercentual(short[] idades) {
-    int total = idades.length;
-    HashMap<String, Integer> totais = calcularQuantidade(idades);
-    
-    String menores = new DecimalFormat("##0.0#").format((totais.get("menores") * 100) / total);
-    String adultos = new DecimalFormat("##0.0#").format((totais.get("adultos") * 100) / total);
-    String idosos = new DecimalFormat("##0.0#").format((totais.get("idosos") * 100) / total);
-    
-    HashMap<String, String> percentuais = new HashMap<>();
+  public static HashMap<String, String> calcularPercentual(ArrayList<Integer> idades) {
+    int total = idades.size();
+    String menores = "0";
+    String adultos = "0";
+    String idosos = "0";
+    HashMap<String, Integer> totais = calcularQuantidades(idades);
+
+    if (idades.size() != 0) {
+      menores = new DecimalFormat("##0.0#").format((float) (totais.get("menores") * 100) / total);
+      adultos = new DecimalFormat("##0.0#").format((float) (totais.get("adultos") * 100) / total);
+      idosos = new DecimalFormat("##0.0#").format((float) (totais.get("idosos") * 100) / total);
+    }
+
+    HashMap<String, String> percentuais = new HashMap<String, String>();
     percentuais.put("menores", menores);
     percentuais.put("adultos", adultos);
     percentuais.put("idosos", idosos);
-    
+
     return percentuais;
   }
+
 }
